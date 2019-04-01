e0b1377df048c9b0683020ae1190507f213fc44d
sverdi <46566997+sverdi@users.noreply.github.com>
2019-04-01 11:53:08 -0400

    start game

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let pos = new Vector(60, 60);
+let vel = new Vector(.5, .2);
+
 function init() {
 
-    alert("hello world");
+    pos.print();
+
     update();
 }
 
 function update() {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    pos.addVector(vel);
+
+    c.beginPath();
+    c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false);
+    c.closePath();
+    c.stroke();
 
     requestAnimationFrame(update);
 }
--- vector.js
+++ vector.js
@@ -1,7 +1,29 @@
 class Vector {
-    
-    constructor() {
 
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    addVector(vec) {
+        this.x+=vec.x;
+        this.y+=vec.y;
+        return this;
+    }
+
+    subVector(vec) {
+
+    }
+
+    scale(s) {
+        
+    }
+
+    toString(){
+        return '<' + this.x+','+this.y+'>'
+    }
+    print() {
+        console.log(this.toString());
     }
 
 }
